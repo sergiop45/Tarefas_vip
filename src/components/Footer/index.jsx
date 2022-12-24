5ddfbbeebb563f4261c92af3ebbb871bf4cc4083
sergiop45 <sergiopsantos33@gmail.com>
2022-12-23 23:46:19 -0300

    header-footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css'
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+        <h1>Tarefas_vip</h1>
+    </footer>
+  )
+}
+
+export default Footer;
